bff1f2fb56523c53a6054243cd4a6fab6e9cf287
Nasrul <nasrul@41studio.com>
2017-07-10 11:27:21 +0700

    initial commit

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  rolify
+  resourcify
+  has_and_belongs_to_many :roles, :join_table => :users_roles
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable, :confirmable, :lockable
+end
